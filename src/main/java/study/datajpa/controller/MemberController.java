cb06977d888da660caf32a1fb04e23b647de236d
sojeongw <sojeongw.official@gmail.com>
2022-05-16 08:12:05 +0900

    Web 확장: 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,6 +1,8 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +28,28 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> listPageMap1(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        Page<MemberDto> pageDto = page.map(MemberDto::new);
+        return pageDto;
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> listPageMap2(Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
---
+++ src/main/java/study/datajpa/controller/MemberDto.java
@@ -0,0 +1,15 @@
+package study.datajpa.controller;
+
+import lombok.Data;
+import study.datajpa.entity.Member;
+
+@Data
+public class MemberDto {
+    private Long id;
+    private String username;
+
+    public MemberDto(Member m) {
+        this.id = m.getId();
+        this.username = m.getUsername();
+    }
+}
