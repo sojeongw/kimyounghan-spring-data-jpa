dcf8477e10bc2ad4055db1e78b4c59ae8a5e782c
sojeongw <sojeongw.official@gmail.com>
2022-05-19 07:47:16 +0900

    나머지 기능들: 명세

---
+++ src/main/java/study/datajpa/repository/MemberSpec.java
@@ -0,0 +1,31 @@
+package study.datajpa.repository;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
+
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+
+public class MemberSpec {
+
+    public static Specification<Member> teamName(final String teamName) {
+
+        // predicate를 만든다.
+        return (Specification<Member>) (root, query, builder) -> {
+
+            if (StringUtils.isEmpty(teamName)) {
+                return null;
+            }
+
+            // 회원과 조인한다.
+            Join<Member, Team> t = root.join("team", JoinType.INNER);
+            return builder.equal(t.get("name"), teamName);
+        };
+    }
+
+    public static Specification<Member> username(final String username) {
+        return (Specification<Member>) (root, query, builder) -> builder.equal(root.get("username"), username);
+    }
+}
